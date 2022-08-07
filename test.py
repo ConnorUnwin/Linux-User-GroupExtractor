ec89a95ebcab45a949c8c8e73c8ed847bf5661dd
ConnorUnwin <contact@connorunwin.com>
2022-08-07 22:15:24 +0100

    Able to return a list of user passwd entry fields

--- test.py
+++ test.py
@@ -1,10 +1,24 @@
 import subprocess as sp
 
-try:
-     user = input("Please enter a username: ")
-     if not user:
-        raise ValueError("Empty input, Username need to be supplied!")
-except ValueError as Error:
-    print(Error)
+def get_username():
+    try:
+        username = input("Please enter a username: ")
+        if not username:
+            raise ValueError("Empty input, Username need to be supplied!")
+        else:
+            passwd_entry = sp.run(['grep', username, '/etc/passwd'], capture_output=True)
+            if passwd_entry.returncode == 1:
+                print("User not found!")
+            elif passwd_entry.returncode == 0:
+                print("User found!")
+                user_lst = passwd_entry.stdout.decode().split(':')
+            else:
+                print("Something went wrong!")
+                print(passwd_entry.returncode)
+    except ValueError as Error:
+        print(Error)
+        username = input("Please enter a username: ")
+    return user_lst
 
-passwd_entry = sp.run(['grep', user, '/etc/passwd'], check=True)
+data = get_username()
+print(data)
